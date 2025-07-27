1f6813cba696019c7cf77308b101eb534449c690
codesubbu <codersubbu@proton.me>
2025-07-27 07:43:16 +0530

    Second push

--- main.go
+++ main.go
@@ -3,5 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var FirstName string
+	var LastName string
+	var Email string
+	var RemSlots int = 50
+
+	var Slots int
+
+	fmt.Println("Enter your first name ")
+	fmt.Scan(&FirstName)
+
+	fmt.Println("Enter your last name ")
+	fmt.Scan(&LastName)
+
+	fmt.Println("Enter your Email ")
+	fmt.Scan(&Email)
+
+	fmt.Println("Enter Slots ")
+	fmt.Scan(&Slots)
+
+	RemSlots = RemSlots - Slots
+	fmt.Printf("Thanks you %v %v for booking %v slots and confirmation has sent to %v\n", FirstName, LastName, Slots, Email)
+	fmt.Println("The remaining slots are ", RemSlots)
+
 }
